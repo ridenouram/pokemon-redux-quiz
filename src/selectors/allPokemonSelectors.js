5424c8f443b237bf03580c07ba24f99178d23929
Anna Ridenour <annamridenour@gmail.com>
2019-05-31 15:10:55 -0700

    wrote selectors

---
+++ src/selectors/allPokemonSelectors.js
@@ -0,0 +1,3 @@
+export const selectAllPokemon = state => state.allPokemon.pokemonArr;
+export const selectAllPokemonLoading = state => state.allPokemon.loading;
+export const selectAllPokemonError = state => state.allPokemon.error;
