37dfe37067f00afd46a281be6d1d726793be8d2a
Anna Ridenour <annamridenour@gmail.com>
2019-05-31 14:48:14 -0700

    wrote reducer, combineReducer

---
+++ src/reducers/allPokemonReducer.js
@@ -0,0 +1,20 @@
+import { ALL_FETCH, ALL_FETCH_LOADING, ALL_FETCH_ERROR } from '../actions/listActions';
+
+const initialState = {
+  loading: false,
+  error: null,
+  pokemonArr: []
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type){
+    case ALL_FETCH_LOADING:
+      return { ...state, loading: true }; 
+    case ALL_FETCH:
+      return { ...state, loading: false, error: null, pokemonArr: action.payload  };
+    case ALL_FETCH_ERROR:
+      return { ...state, error: action.payload };
+    default:
+      return state;
+  }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux';
+import allPokemon from './allPokemonReducer';
+
+export default combineReducers({
+  allPokemon
+});
