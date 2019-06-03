1aed26c2489a6613789b79dc1b93b6f56a66fbc6
Anna Ridenour <annamridenour@gmail.com>
2019-06-02 18:15:51 -0700

    wrote selectors for pokemonDEtail

---
+++ src/containers/PokemonDetail.js
@@ -0,0 +1,4 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import 
---
+++ src/selectors/pokemonDetailSelectors.js
@@ -0,0 +1,3 @@
+export const selectOnePokemon = state => state.pokemonDetail.pokemon;
+export const selectOnePokemonLoading = state => state.pokemonDetail.loading;
+export const selectOnePokemonError = state => state.pokemonDetail.error;
