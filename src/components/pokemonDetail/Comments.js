8d781d4bc13579936fa76add66bf92b161704aab
Anna Ridenour <annamridenour@gmail.com>
2019-06-03 10:44:14 -0700

    wrote Comments

---
+++ src/components/pokemonDetail/Comments.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Comment from './Comment';
+
+function Comments({ comments }) {
+  const commentList = comments.map((comment, i) => {
+    return (
+      <li key={i}>
+        <Comment comment={comment} />
+      </li>
+    );
+  });
+
+  return (
+    <ul>
+      {commentList}
+    </ul>
+  );
+}
+
+Comments.propTypes = {
+  comments: PropTypes.array.isRequired
+};
+
+export default Comments;
