7dd5bf621a4aa9eaff7feca3392e00757cf7cfbe
Anna Ridenour <annamridenour@gmail.com>
2019-06-02 18:51:17 -0700

    added router to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import AllPokemon from '../containers/AllPokemon';
+import PokemonDetail from '../containers/PokemonDetail';
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 
 export default function App() {
   return (
-    <AllPokemon />
+    <Router>
+      <Link to="/">All Pokemon</Link>
+      <Switch>
+        <Route exact path="/" component={AllPokemon} />
+        <Route path="/:id" component={PokemonDetail} />
+      </Switch>
+    </Router>
   );
 }
